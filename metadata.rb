0eeb802da2c1d5eb9a113109f4461490d564896b
Wes Mason <wes@1stvamp.org>
2013-02-13 16:10:15 +0000

    Add more metadata including dependencies

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,21 @@
+name               "serverdensity"
 maintainer         "Server Density"
 maintainer_email   "hello@serverdensity.com"
 license            "MIT"
-description        "Installs/Configures Server Density sd-agent"
+description        "Installs/configures Server Density sd-agent"
 long_description   IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version            "1.0.0"
+
+depends "apt"
+depends "yum"
 
 supports "ubuntu"
 supports "debian"
 supports "centos"
 supports "redhat"
 supports "fedora"
+supports "amazon"
+supports "scientific"
+
+recipe "serverdensity::default", "Default"
+recipe "serverdensity::install", "Installs, configures and starts sd-agent"
