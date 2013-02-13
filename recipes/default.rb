ac959081c884dc9f4891aacece4f6ed2f975b294
Wes Mason <wes@1stvamp.org>
2013-02-13 09:13:33 +0000

    Normalise indentation and use 2 spaces rather than tabs

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,6 @@
 #
 # Cookbook Name:: serverdensity
 # Recipe:: default
-#
-# Copyright 2012, Escape Studios
-#
 
 include_recipe "serverdensity::install"
 include_recipe "serverdensity::server-monitor"
--- recipes/server-monitor.rb
+++ recipes/server-monitor.rb
@@ -1,29 +1,26 @@
 #
 # Cookbook Name:: serverdensity
 # Recipe:: server-monitor
-#
-# Copyright 2012, Escape Studios
-#
 
 #install the server monitor
 package "sd-agent" do
-	action :install
+  action :install
 end
 
 #configure your Server Density agent key
 template "/etc/sd-agent/config.cfg" do
-	source "config.cfg.erb"
-	owner "root"
-	group "root"
-	mode "644"
-	variables(
-		:sd_url => node[:serverdensity][:sd_url],
-		:agent_key => node[:serverdensity][:agent_key]
-	)
-	notifies :restart, "service[sd-agent]"
+  source "config.cfg.erb"
+  owner "root"
+  group "root"
+  mode "644"
+  variables(
+    :sd_url => node[:serverdensity][:sd_url],
+    :agent_key => node[:serverdensity][:agent_key]
+  )
+  notifies :restart, "service[sd-agent]"
 end
 
 service "sd-agent" do
   supports :start => true, :stop => true, :restart => true
-	action [:enable, :start] #starts the service if it's not running and enables it to start at system boot time
+  action [:enable, :start] #starts the service if it's not running and enables it to start at system boot time
 end
