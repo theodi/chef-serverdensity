fba0d98571e1428c3653c2c67d6f3aa0a6eac0d9
Mal Graty <mal.graty@googlemail.com>
2013-11-16 20:23:54 +0000

    Add configured? method

--- libraries/api.rb
+++ libraries/api.rb
@@ -14,10 +14,15 @@ module ServerDensity
         case version.to_i
           when 1 then class << self; include V1; end
           when 2 then class << self; include V2; end
+          else raise 'Invalid Server Density API version'
         end
         @version = version
         initialize(*args)
         self
+      end
+
+      def configured?
+        not @version.nil?
       end
     end
 
