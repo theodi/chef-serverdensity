00dc13d278ccb4c11670ae957bf7c7907fb8881d
Mal Graty <mal.graty@googlemail.com>
2013-11-16 18:57:44 +0000

    Cut down on comments in attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -38,9 +38,7 @@ default['serverdensity']['plugin_dir'] = ""
 
 # Set this to enable Apache monitoring
 default['serverdensity']['apache_status_url'] = nil
-# Change apache_status_user if you set apache_status_url above
 default['serverdensity']['apache_status_user'] = ""
-# Change apache_status_pass if you set apache_status_url above
 default['serverdensity']['apache_status_pass'] = ""
 
 # Set this to enable MongoDB monitoring
@@ -50,9 +48,7 @@ default['serverdensity']['mongodb_replset'] = false
 
 # Set this to enable MySQL monitoring
 default['serverdensity']['mysql_server'] = nil
-# Change mysql_user if you set mysql_server above
 default['serverdensity']['mysql_user'] = ""
-# Change mysql_pass if you set mysql_server above
 default['serverdensity']['mysql_pass'] = ""
 
 # Set this to enable nginx monitoring
@@ -60,9 +56,7 @@ default['serverdensity']['nginx_status_url'] = nil
 
 # Set this to enable RabbitMQ monitoring
 default['serverdensity']['rabbitmq_status_url'] = nil
-# Will default['to guest
 default['serverdensity']['rabbitmq_user'] = nil
-# Will default['to guest
 default['serverdensity']['rabbitmq_pass'] = nil
 
 # temp directory location, otherwise system default is used if not set
