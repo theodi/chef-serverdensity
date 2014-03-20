7a4e97097c19ec034fc4447ef33bbc566690ecfd
David Mytton <david@mytton.net>
2014-03-20 15:59:49 +0000

    Bump version up to 2.0.0 for official release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email   "hello@serverdensity.com"
 license            "MIT"
 description        "Installs/configures Server Density sd-agent"
 long_description   IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version            "1.0.1"
+version            "2.0.0"
 
 depends "apt"
 depends "yum"
